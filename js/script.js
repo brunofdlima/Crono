e809d04e6d1335c54fbb855ca582479885b55efa
Bruno Ferreira <brunofor14@gmail.com>
2022-05-22 03:40:56 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,62 @@
+var centesimos = 0;
+var segundos = 0;
+var minutos = 0;
+var horas = 0;
+function inicio () {
+    control = setInterval(cronometro,10);
+    document.getElementById("inicio").disabled = true;
+    document.getElementById("parar").disabled = false;
+    document.getElementById("continuar").disabled = true;
+    document.getElementById("reinicio").disabled = false;
+}
+function parar () {
+    clearInterval(control);
+    document.getElementById("parar").disabled = true;
+    document.getElementById("continuar").disabled = false;
+}
+function reinicio () {
+    clearInterval(control);
+    centesimos = 0;
+    segundos = 0;
+    minutos = 0;
+    horas = 0;
+    Centesimas.innerHTML = ":00";
+    Segundos.innerHTML = ":00";
+    Minutos.innerHTML = ":00";
+    Horas.innerHTML = "00";
+    document.getElementById("inicio").disabled = false;
+    document.getElementById("parar").disabled = true;
+    document.getElementById("continuar").disabled = true;
+    document.getElementById("reinicio").disabled = true;
+}
+function cronometro () {
+    if (centesimos < 99) {
+        centesimos++;
+        if (centesimos < 10) { centesimos = "0"+centesimos }
+        Centesimas.innerHTML = ":"+centesimos;
+    }
+    if (centesimos == 99) {
+        centesimos = -1;
+    }
+    if (centesimos == 0) {
+        segundos ++;
+        if (segundos < 10) { segundos = "0"+segundos }
+        Segundos.innerHTML = ":"+segundos;
+    }
+    if (segundos == 59) {
+        segundos = -1;
+    }
+    if ( (centesimos == 0)&&(segundos == 0) ) {
+        minutos++;
+        if (minutos < 10) { minutos = "0"+minutos }
+        Minutos.innerHTML = ":"+minutos;
+    }
+    if (minutos == 59) {
+        minutos = -1;
+    }
+    if ( (centesimos == 0)&&(segundos == 0)&&(minutos == 0) ) {
+        horas ++;
+        if (horas < 10) { horas = "0"+horas }
+        Horas.innerHTML = horas;
+    }
+}
